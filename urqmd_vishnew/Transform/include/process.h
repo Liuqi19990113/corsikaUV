9acf19a5f9948aedab6b7e1199871a542f04a220
AlphaNotKnows <qinjian.lou@Outlook.com>
2020-12-13 11:33:03 +0800

    add urqmd_vishnew

---
+++ urqmd_vishnew/Transform/include/process.h
@@ -0,0 +1,47 @@
+#ifndef _PROCESS_H_
+#define _PROCESS_H_
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<cmath>
+#include <string>
+#include <sstream>
+#include "transform.h"
+#include "Vector4.h"
+#include "Particle.h"
+#include "EPTensor.h"
+namespace Transform{
+
+  /**print the result in 3 dim
+   * @ para[in] output_file : output file name
+   * @ para[in] tensor : result energy tensor
+  */
+  void print3(const std::string&output_file,const EPTensor&tensor);
+  /**do Lorentz transformation from (p[0],p[1],p[2],p[3]) to (E,px,py,pz)*/
+  void LorentzTransform(const double beta[4],double p[4]);
+  /** remove the file header in urqmd file*/
+  void remove_header(std::istream&input);
+  /** calculate the sum of momentum and get (beta[0],beta)
+   * @para[in] beta: the vector to store beta
+   * @para[in] input_file : the name of the data file
+  */
+  void CalBeta(double beta[4],const std::string&input_file=Ex_input_file);
+  /** search the particle on tau_0
+   * @para[in] secondaries : the vector<Particle> which store all particles on tau_0
+   * @para[in] input_file : the name of the data file
+   * @ return the secondaries number at last time
+   */
+  unsigned search_tau(std::vector<Particle>&secondaries,const std::string&input_file=Ex_input_file);
+  /** count the number of secondaries in each time step*/
+  void secondaries_count(const std::string&input_file,const std::string&output_file);
+  /** search the spectator
+   * @para[in] secondaries : the vector store all secondaries
+   * @para[in] output_path : the path to output the secondaries
+   * @note the spectator will be removed from the secondaries
+  */
+  void search_spectator(std::vector<Particle>&secondaries,const std::string&output_path=Ex_output_path);
+  /**search the energy at t0*/
+  double search_energy(const std::string &input_file=Ex_input_file);
+}
+
+#endif
