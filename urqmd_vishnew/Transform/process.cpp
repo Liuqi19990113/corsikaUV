bd7d733125d83602e4cce9654e7e57570d157a90
AlphaNotKnows <qinjian.lou@Outlook.com>
2021-01-08 15:42:13 +0800

    remove LorentzTransform in transform

--- urqmd_vishnew/Transform/process.cpp
+++ urqmd_vishnew/Transform/process.cpp
@@ -57,7 +57,7 @@ namespace Transform{
   
   unsigned search_tau(std::vector<Particle>&secondaries,const std::string&input_file){
     double beta[4]={0};
-    CalBeta(beta);
+    // CalBeta(beta);
     std::ifstream input(input_file.c_str());
     std::string data_line;
     //remove the header
@@ -88,8 +88,8 @@ namespace Transform{
         for(unsigned j=0;j<4;j++)input_line>>x[j];
         for(unsigned j=0;j<4;j++)input_line>>p[j];
         input_line>>mass>>itype;
-        LorentzTransform(beta,p);
-        LorentzTransform(beta,x);
+        // LorentzTransform(beta,p);
+        // LorentzTransform(beta,x);
         for(int k=0;k<4;k++){
           p_sum[k]+=p[k];
         }
@@ -177,7 +177,7 @@ namespace Transform{
 
   double search_energy(const std::string &input_file){
     double beta[4]={0};
-    CalBeta(beta);
+    // CalBeta(beta);
     std::ifstream input(input_file.c_str());
     std::string data_line;
     //remove the header
@@ -205,8 +205,8 @@ namespace Transform{
         int itype;
         for(unsigned j=0;j<4;j++)input_line>>x[j];
         for(unsigned j=0;j<4;j++)input_line>>p[j];
-        LorentzTransform(beta,p);
-        LorentzTransform(beta,x);
+        // LorentzTransform(beta,p);
+        // LorentzTransform(beta,x);
         input_line>>mass>>itype;
         energy+=p[0];
         for(int k=1;k<4;k++){
